c83ed63e3d7db5599dec431f3da19a2ccd3e0c26
eviefontaine <63077631+eviefontaine@users.noreply.github.com>
2021-07-14 14:32:02 -0400

    2nd Version

--- StoryboardIntro/StoryboardIntro/ViewController.swift
+++ StoryboardIntro/StoryboardIntro/ViewController.swift
@@ -9,6 +9,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var appTitle: UILabel!
+    @IBOutlet weak var textField: UITextField!
+    @IBAction func button(_ sender: UIButton) {
+        if let button1 = textField.text {
+            appTitle.text = button1
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
